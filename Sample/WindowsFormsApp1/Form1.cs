de8fa7f635b0d0da99b0e5c7b141c86b356fe5b1
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-04-17 10:52:19 +0900

    不要部分削除

--- Sample/WindowsFormsApp1/Form1.cs
+++ Sample/WindowsFormsApp1/Form1.cs
@@ -16,7 +16,6 @@ namespace WindowsFormsApp1 {
 
         private void btButton_Click(object sender, EventArgs e) {
             //tbAns.Text = (int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text)).ToString();
-
             int num1 = int.Parse(tbNum1.Text);
             int num2 = int.Parse(tbNum2.Text);
             int sum = num1 + num2;
