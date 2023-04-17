4e47a441e89b47fcdc5430ccd5cd15b888d32fb0
infosys <infosys@C4216>
2023-04-17 10:06:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample/ConsoleApp0411/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0411 {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] moneyString = { "一万円", "五千円", "二千円", "千円", "五百円", "百円", "五十円", "十円", "五円", "一円", };
+            int[] moneyInteger = { 10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1 };
+
+            Console.Write("合計金額");
+            int Total = int.Parse(Console.ReadLine());
+            Console.Write("支払い金額");
+            int Pay = int.Parse(Console.ReadLine());
+
+            int Money = Pay - Total;
+
+            Console.Write("おつり");
+            Console.WriteLine(Money);
+
+            for (int i = 0; i < moneyString.Length; i++)
+            {
+                //Console.WriteLine(moneyString[i] + ":{0}枚", Money / moneyInteger[i]);
+                Console.Write(moneyString[i] + ":");
+                astOut(Money / moneyInteger[i]);
+                Money %= moneyInteger[i];
+            }
+
+            //直角三角
+            //for (int i = 0; i < Count; i++)
+            //{
+            //    //空白を出力
+            //    for (int j = 0; j < Count - (i + 1); j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    //＊を出力
+            //    for (int k = 0; k < i + 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();    //改行
+            //}
+
+            //二等辺三角
+            //for (int i = 0; i < Count; i++)
+            //{
+            //    //空白を出力
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    //＊を出力
+            //    for (int k = 0; k < Count - i; k++)
+            //    {
+            //        Console.Write("*");
+            //        Console.Write(" ");
+
+            //        //＊全角でもOK
+            //        //Console.Write("＊");
+            //    }
+            //    Console.WriteLine();    //改行
+            //}
+        }
+
+        //指定した個数の＊を表示
+        private static void astOut(int count) {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sample/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btButton = new System.Windows.Forms.Button();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.tbAns = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btButton
+            // 
+            this.btButton.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btButton.Location = new System.Drawing.Point(346, 248);
+            this.btButton.Name = "btButton";
+            this.btButton.Size = new System.Drawing.Size(126, 74);
+            this.btButton.TabIndex = 0;
+            this.btButton.Text = "計算";
+            this.btButton.UseVisualStyleBackColor = true;
+            this.btButton.Click += new System.EventHandler(this.btButton_Click);
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNum1.Location = new System.Drawing.Point(141, 46);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(126, 34);
+            this.tbNum1.TabIndex = 1;
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNum2.Location = new System.Drawing.Point(346, 46);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(126, 34);
+            this.tbNum2.TabIndex = 1;
+            // 
+            // tbAns
+            // 
+            this.tbAns.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbAns.Location = new System.Drawing.Point(535, 46);
+            this.tbAns.Name = "tbAns";
+            this.tbAns.Size = new System.Drawing.Size(126, 34);
+            this.tbAns.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(292, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(494, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAns);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.btButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btButton;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox tbAns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
