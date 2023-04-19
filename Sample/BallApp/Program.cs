685f6a5fef51d4bfca9c511194c16346073aa530
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-04-19 11:12:18 +0900

    マウスクリック時にボールをマウスポインターの位置で生成

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -8,9 +8,9 @@ using System.Windows.Forms;
 
 namespace BallApp {
     class Program :Form{
-        private Timer moveTimer;                        //タイマー用
-        SoccerBall soccerBall   = new SoccerBall();
-        PictureBox pb           = new PictureBox();     //画像を表示するコントロール
+        private Timer moveTimer;                            //タイマー用
+        SoccerBall soccerBall   = new SoccerBall(0.0,0.0);
+        PictureBox pb           = new PictureBox();         //画像を表示するコントロール
 
         static void Main(string[] args) {
             Application.Run(new Program());
@@ -22,23 +22,30 @@ namespace BallApp {
             this.Size       = new Size(800, 600);
             this.BackColor  = Color.Blue;
             this.Text       = "BallGame";
-            
-            //ボールインスタンス生成
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);    //画像の位置
-            pb.Size     = new Size(50, 50);                                         //画像の表示サイズ
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;                          //画像の表示モード
-            
-            pb.Parent = this;
+            this.MouseClick += Program_MouseClick;
             
             moveTimer           = new Timer();
             moveTimer.Interval  = 20;               //タイマーのインターバル（ｍｓ）   
-            moveTimer.Start();                      //タイマースタート
-            moveTimer.Tick      += MoveTimer_Tick;
+            moveTimer.Tick      += MoveTimer_Tick;  //デリゲート登録
         }
 
+        //マウスクリック時のイベントハンドラ
+        private void Program_MouseClick(object sender, MouseEventArgs e) {
+            //ボールインスタンス生成
+            soccerBall      = new SoccerBall(e.X - 25,e.Y - 25);
+            pb              = new PictureBox();
+            pb.Image        = soccerBall.Image;
+            pb.Location     = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);//画像の位置
+            pb.Size         = new Size(50, 50);                                     //画像の表示サイズ
+            pb.SizeMode     = PictureBoxSizeMode.StretchImage;                      //画像の表示モード
+            moveTimer.Start();                                                      //タイマースタート
+
+            pb.Parent       = this;
+        }
+
+        //タイマータイムアウト時のイベントハンドラ
         private void MoveTimer_Tick(object sender, EventArgs e) {
-            soccerBall.Move();      //移動
+            soccerBall.Move();                                                      //移動
             pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);    //画像の位置
         }
     }
--- Sample/BallApp/SoccerBall.cs
+++ Sample/BallApp/SoccerBall.cs
@@ -13,16 +13,28 @@ namespace BallApp {
         private double posX;            //x座標
         private double posY;            //y座標
 
-        private double moveX = 10.0;     //移動量（ｘ方向）
-        private double moveY = 10.0;     //移動量（ｙ方向）
-        
+        private double moveX;           //移動量（ｘ方向）
+        private double moveY;           //移動量（ｙ方向）
+
+        Random random = new Random();   //乱数インスタンス
+
         //コンストラクタ
-        public SoccerBall() {
-            Image   = Image.FromFile(@"pic\soccer_ball.png");
-            PosX    = 0.0;
-            PosY    = 0.0;
+        public SoccerBall(double mouse_posX, double mouse_posY) {
+
+            int speedX = random.Next(-20, 20);
+            int speedY = random.Next(-20, 20);
+            if (speedX == 0)
+                speedX = random.Next(1, 20);
+            if(speedY==0)
+                speedY = random.Next(1, 20);
+
+            Image = Image.FromFile(@"pic\soccer_ball.png");
+            PosX = mouse_posX;
+            PosY = mouse_posY;
+            moveX = speedX;
+            moveY = speedY;
         }
-        
+
         //プロパティ
         public double PosX { get => posX; set => posX = value; }
         public double PosY { get => posY; set => posY = value; }
@@ -35,8 +47,8 @@ namespace BallApp {
             if (posY > 500 || posY < 0)     //Y座標の壁判定
                 moveY *= -1;
 
-            posX += moveX;
-            posY += moveY;
+            posX    += moveX;
+            posY    += moveY;
         }
     }
 }
