e77645902c1b05368083cdff02596b16cf5d10a5
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-08 10:54:36 +0900

    p26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    class Program {
+        static void Main(string[] args) {
+            //インスタンスの生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daihuku = new Product(235, "大福もち", 160);
+            
+            Console.WriteLine("かりんとうの税込価格：" + karinto.GetPriceIncludingTax());
+            Console.WriteLine("大福もちの税込価格：" + daihuku.GetPriceIncludingTax());
+        
+        }
+    }
+}
