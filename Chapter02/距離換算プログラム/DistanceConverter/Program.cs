bdb89644cdc59964484fed40c146df45af1f0232
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-10 09:34:22 +0900

    機能を追加（メートルからフィート変換）

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -7,17 +7,33 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
-            //フィートからメートルへの対応表を出力
-            for (int feet = 1; feet <= 10; feet++)
+            if (args.Length >= 1 && args[0] == "-tom")
             {
-                double meter = FeetToMeter(feet);
-                Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
+                //フィートからメートルへの対応表を出力
+                for (int feet = 1; feet <= 10; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
+                }
+            }
+            else
+            {
+                //メートルからフィートへの対応表を出力
+                for (int meter = 1; meter <= 10; meter++)
+                {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
+                }
             }
         }
 
         //フィートからメートルを求める
         static double FeetToMeter(int feet) {
             return feet * 0.3048;
+        }
+        //メートルからフィートを求める
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
         }
     }
 }
