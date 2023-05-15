b05cdfd401a0037d27fa8e1c065d9152e366ce91
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-15 12:09:09 +0900

    店舗別に売上を求める処理追加

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -8,6 +8,11 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
+            SalesCounter sales = new SalesCounter(ReadSales(@"date\sales.csv"));
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string, int> obj in amountPerStore) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            }
         }
 
         //売上データを読み込み、Saleオブジェクトのリストを返す
---
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    public class SalesCounter {
+
+        private List<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        //店舗別売上を求める
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount; //店名が既に存在する（売上加算）
+                else
+                    dict[sale.ShopName] = sale.Amount; //店名が存在しない（新規格納）
+            }
+            return dict;
+        }
+    }
+}
