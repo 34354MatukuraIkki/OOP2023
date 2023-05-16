742bb4b10c43abd1001d2ab19c7f676a989dc765
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-16 10:39:51 +0900

    SalesCounterのコンストラクタ修正

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"date\sales.csv"));
+            SalesCounter sales = new SalesCounter(@"date\sales.csv");
             Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -11,8 +11,8 @@ namespace SalesCalculator {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
         //店舗別売上を求める
@@ -28,7 +28,7 @@ namespace SalesCalculator {
         }
 
         //売上データを読み込み、Saleオブジェクトのリストを返す
-        public static List<Sale> ReadSales(string filePath) {
+        private static List<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();    //売上データを格納する
             string[] lines = File.ReadAllLines(filePath);   //ファイルからすべてのデータを読み込む
 
