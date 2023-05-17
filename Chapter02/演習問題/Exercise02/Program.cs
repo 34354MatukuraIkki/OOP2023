9fe161bde4f0c7597597fbe343d99b65963fe9e0
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-17 12:09:48 +0900

    演習問題2.2

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length < 3)
+                return;
+
+            if (args[0] == "-tom") {
+                PrintInchToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+            else if (args[0] == "-toi") {
+                PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+        }
+
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} ih = {1:0.0000}m", inch, meter);
+            }
+        }
+
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000}ih", meter, inch);
+            }
+        }
+    }
+}
