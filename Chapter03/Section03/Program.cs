384363c4d90dca5716ff442402dda037d2dc7547
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-23 11:12:46 +0900

    List<T>クラスとラムダ式の組み合わせ

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -10,6 +10,21 @@ namespace Section03 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //var exists = list.Exists(s => s[3] == 'd');
+            //Console.WriteLine(exists);
+
+            //var name = list.Find(s => s.Length == 4);
+            //Console.WriteLine(name);
+
+            //var index = list.FindIndex(s => s == "Paris");
+            //Console.WriteLine(index);
+
+            //var names = list.FindAll(s => s.Length <= 7);
+            //names.ForEach(s => Console.WriteLine(s));
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
         }
     }
 }
