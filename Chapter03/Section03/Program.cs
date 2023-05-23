d5c617a575fb558041e34196c536ff1dc6fbbf40
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-23 10:36:20 +0900

    Section03新規作成

---
+++ Chapter03/Section03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+        }
+    }
+}
