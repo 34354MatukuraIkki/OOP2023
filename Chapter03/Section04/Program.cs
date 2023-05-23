4977897e57599ebd95deb5f9e22dbe9489af6b00
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-23 11:54:18 +0900

    第三章演習問題１

---
+++ Chapter03/Section04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //var query = names.FindAll(s => s.Length <= 5).Select(s => s.ToLower());
+            var query = names.Select(s => s.Length);
+            foreach (var s in query) {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
