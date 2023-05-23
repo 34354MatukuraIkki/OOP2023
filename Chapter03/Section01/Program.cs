a2b94cb5b7d88128bfeeebf0b8fe038b88db169f
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-23 10:30:44 +0900

    ラムダ式を使った例

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,20 +8,14 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+            //５の倍数をカウント
+            //int count = numbers.Count(n => n % 5 == 0);
+
+            //偶数の合計値
+            var count = numbers.Where(n => n % 2 == 0).Sum();
             
-            int count = Count(numbers, delegate (int n) { return n % 2 == 0; });
             Console.WriteLine(count);
-        }
-        
-        static public int Count(int[] numbers, Predicate<int> judge) {
-            int count = 0;
-
-            foreach (var n in numbers) {
-                if (judge(n) == true)
-                    count++;
-            }
-
-            return count;
         }
     }
 }
