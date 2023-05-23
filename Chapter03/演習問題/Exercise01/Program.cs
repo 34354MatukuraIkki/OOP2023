44bbf5c45337b63a2b49a09f131491c59b115caf
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-23 12:08:03 +0900

    Exercise01完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -26,7 +26,13 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_1(List<int> numbers) {
-        
+            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+
+            if (exists==true) 
+                Console.WriteLine("存在しています");
+            else
+                Console.WriteLine("存在していません");
+
         }
 
         private static void Exercise1_2(List<int> numbers) {
