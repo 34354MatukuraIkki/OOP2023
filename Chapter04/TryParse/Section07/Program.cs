3cfe4f4256e6ce067a38c14d635f6bc0e4a4d565
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-29 12:09:21 +0900

    TryParse

---
+++ Chapter04/TryParse/Section07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section07 {
+    class Program {
+        static void Main(string[] args) {
+
+            var str = "123";
+            int height;
+
+            if (int.TryParse(str, out height)) {
+                //heightには変換された値が入っている
+                Console.WriteLine(height);
+            }
+            else {
+                //変換に失敗した
+                Console.WriteLine("変換できません");
+            }
+        }
+    }
+}
