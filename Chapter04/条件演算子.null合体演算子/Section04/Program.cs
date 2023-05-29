9d755e48c8f7136c7519249903f445b05002a47e
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-29 11:57:02 +0900

    null条件演算子

--- Chapter04/条件演算子.null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子.null合体演算子/Section04/Program.cs
@@ -16,18 +16,37 @@ namespace Section04 {
             #endregion
 
             #region null合体演算子
-            string code = "12345";
-            var message = GetMessage(code) ?? DefaultMessage();
-            Console.WriteLine(message);
+            //string code = "12345";
+            //var message = GetMessage(code) ?? DefaultMessage();
+            //Console.WriteLine(message);
+            #endregion
+
+            #region null条件演算子
+            Sale sale = null;
+
+            //「int?」はnull許容型、「?.」はnull条件演算子
+            int? ret = sale?.Amount;
+
+            Console.WriteLine(ret);
             #endregion
         }
 
         private static object GetMessage(object code) {
             return null;
         }
-
         private static object DefaultMessage() {
             return "Default Message";
         }
+    }
+
+    //売上クラス
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+
     }
 }
