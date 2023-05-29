a00057926cf62c0d2bae6e4aae815cb944cbad68
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-29 10:54:16 +0900

    条件演算子

---
+++ Chapter04/条件演算子.null合体演算子/Section04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            #region 条件演算子
+            var list = new List<int> { 10, 20, 30, 40, };
+            var key = 40;
+
+            var num = list.Contains(key) ? 1 : 0;   //条件演算子・三項演算子
+            Console.WriteLine(num);
+            #endregion
+
+        }
+    }
+}
