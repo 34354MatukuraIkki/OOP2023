26e0f9b6ac03a0aa1e23df155886451120997827
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-30 12:09:02 +0900

    4章問題4.1

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    // 4.1.1
+    public class YearMonth {
+        public int Year { get; private set; }
+
+        public int Month { get; private set; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        // 4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        // 4.1.3
+        public YearMonth AddOneMonth() {
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            }
+            else {
+                return new YearMonth(Year, Month + 1);
+            }
+
+            //1行
+            //return new YearMonth(Month == 12 ? Year + 1 : Year, Month == 12 ? 1 : Month + 1);
+        }
+
+        // 4.1.4
+        public override string ToString() {
+            return Year + "年" + Month + "月";
+        }
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var ym = new YearMonth(2023,8);
+            var c21 = ym.Is21Century;
+            var ymNextMonth = ym.AddOneMonth();
+
+            Console.WriteLine(ym);
+            Console.WriteLine(ymNextMonth);
+        }
+    }
+}
