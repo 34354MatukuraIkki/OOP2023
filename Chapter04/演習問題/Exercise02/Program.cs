63bf89b6d6bcfaae64d803bfaf103b03b3999940
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-05-31 09:53:21 +0900

    問題4.2.2

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -9,12 +9,45 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
-            var ym = new YearMonth(2023,8);
-            var c21 = ym.Is21Century;
-            var ymNextMonth = ym.AddOneMonth();
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
 
-            Console.WriteLine(ym);
-            Console.WriteLine(ymNextMonth);
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        //static YearMonth FindFirst21C(YearMonth[] yms) {
+            
+        //}
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+
         }
     }
 }
