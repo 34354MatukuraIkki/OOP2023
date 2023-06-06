3c857bea2cbf5190d726c2e11237398de3a9e4d9
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-06-06 10:17:51 +0900

    問題5.4.1

--- Chapter05/練習問題/Exercise04/Program.cs
+++ Chapter05/練習問題/Exercise04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,28 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-            var text = "NoveList=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+#if true
+            var line = "NoveList=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var texts = line.Split(';');
+            string[] words = { "NoveList=", "BestWork=", "Born=" };
+            string[] label = { "作家　：", "代表作：", "誕生年：" };
+            int num = 0;
+            foreach (var word in texts) {
+                var index = word.IndexOf(words[num]) + words[num].Length;
+                Console.WriteLine(label[num] + word.Substring(index));
+                num++;
+            }
+
+#else
+            string[] lines = {
+                "NoveList=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "NoveList=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "NoveList=太宰治;BestWork=人間失格;Born=1909",
+                "NoveList=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
 
 
+#endif
         }
     }
 }
