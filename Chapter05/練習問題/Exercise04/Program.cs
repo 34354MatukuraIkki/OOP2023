7bf582fb38a48fd1936cbeb0c71141a08b2a78ab
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-06-06 11:14:31 +0900

    問題5.4.2

--- Chapter05/練習問題/Exercise04/Program.cs
+++ Chapter05/練習問題/Exercise04/Program.cs
@@ -1,6 +1,6 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-#if true
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+#if false
             var line = "NoveList=谷崎潤一郎;BestWork=春琴抄;Born=1886";
             var texts = line.Split(';');
             string[] words = { "NoveList=", "BestWork=", "Born=" };
@@ -28,8 +30,46 @@ namespace Exercise04 {
                 "NoveList=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
             };
 
+            //string[] texts = { };
 
+            //foreach (var item in lines) {
+            //    texts = item.Split(';');
+            //}
+
+            //string[] words = { "NoveList=", "BestWork=", "Born=" };
+            //string[] label = { "作家　：", "代表作：", "誕生年：" };
+            //int num = 0;
+            //int labelNum = 0;
+            //foreach (var word in texts) {
+            //    var index = word.IndexOf(words[num]) + words[num].Length;
+            //    Console.WriteLine(label[num] + word.Substring(index));
+            //    num++;
+            //    labelNum++;
+            //    if (labelNum == 4)
+            //        labelNum = 0;
+            //    }
+
+            foreach (var line in lines) {
+                foreach (var pair in line.Split(';')) {
+                    var array = pair.Split('=');
+                    Console.WriteLine("{0},{1}", ToJapanese(array[0]), array[1]);
+                }
+            }
 #endif
+            Console.WriteLine("実行時間" + sw.Elapsed.ToString(@"ss\.ffff"));
+        }
+
+        static string ToJapanese(string key) {
+            switch (key) {
+                case "NoveList":
+                    return "作家　：";
+                case "BestWork":
+                    return "代表作：";
+                case "Born":
+                    return "誕生年：";
+
+            }
+            throw new AggregateException("正しい引数ではありません");
         }
     }
 }
