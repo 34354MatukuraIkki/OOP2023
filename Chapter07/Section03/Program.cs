f4fc85c49068e4e91071f0dbe60c97ea00980e7b
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-06-29 10:21:00 +0900

    Section03作成

---
+++ Chapter07/Section03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+
+            var abbrs = new Abbreviations();
+
+            //Addメソッドのの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //インデクサの利用例
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach (var name in names) {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                    Console.WriteLine("{0}は見つかりません", name);
+                else
+                    Console.WriteLine("{0}={1}", name, fullname);
+            }
+            Console.WriteLine();    //改行
+
+            //ToAbbreviationのメソッドの利用例
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+                Console.WriteLine("{0}は見つかりません", japanese);
+            else
+                Console.WriteLine("{0}={1}", japanese, abbreviation);
+            Console.WriteLine();
+
+            //FillAllメソッドの利用例
+            foreach (var item in abbrs.FindAll("国際")) {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
