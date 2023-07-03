f066670f396f21fcdd2f4050afc96ee48717d5cf
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-07-03 13:53:06 +0900

    問題8.2完成

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -10,11 +10,13 @@ namespace Exercise02 {
         static void Main(string[] args) {
             var date = DateTime.Now;
             var culture = new CultureInfo("ja-JP");
-            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
-            var dayOfWeek = culture.DateTimeFormat.GetDayName(date.DayOfWeek);
 
-            DateTime nextDayOfWeek = NextDay(date, date.DayOfWeek);
-            Console.WriteLine("{0:yyyy/M/d}の次週の{1}: {2:yyyy/M/d} ({3})", date, date.DayOfWeek,nextDayOfWeek,dayOfWeek);
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+                culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+                var dayOfWeek = culture.DateTimeFormat.GetDayName((DayOfWeek)dayofweek);
+                DateTime nextDayOfWeek = NextDay(date, (DayOfWeek)dayofweek);
+                Console.WriteLine("{0:yyyy/M/d}の次週の{1}: {2:yyyy/M/d} ({3})", date, dayofweek, nextDayOfWeek, dayOfWeek);
+            }
         }
         public static DateTime NextDay(DateTime date,DayOfWeek dayOfWeek) {
             var days = (int)dayOfWeek - (int)(date.DayOfWeek);
