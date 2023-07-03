ec0169c1da89b0bd06fcfd06a1be695fa8f11a7a
松倉 一輝 <oibc34354@ojs.ac.jp>
2023-07-03 13:45:28 +0900

    問題8.2修正

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
             var date = DateTime.Now;
-            DateTime nextDayOfWeek = NextDay(date, DayOfWeek.Monday);
-            Console.WriteLine("{0:yyyy/M/d}の次週の{1}: {2:yyyy/M/d}", date, date.DayOfWeek,nextDayOfWeek);
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(date.DayOfWeek);
+
+            DateTime nextDayOfWeek = NextDay(date, date.DayOfWeek);
+            Console.WriteLine("{0:yyyy/M/d}の次週の{1}: {2:yyyy/M/d} ({3})", date, date.DayOfWeek,nextDayOfWeek,dayOfWeek);
         }
         public static DateTime NextDay(DateTime date,DayOfWeek dayOfWeek) {
             var days = (int)dayOfWeek - (int)(date.DayOfWeek);
