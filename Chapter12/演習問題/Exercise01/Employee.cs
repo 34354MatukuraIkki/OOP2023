8bdccf9f01bcf2ff1dd8ee5c85fe103a7729d8d3
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-04 14:24:18 +0900

    問題12.1.1

---
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    [XmlRoot("employee")]
+    public class Employee {
+        [XmlElement(ElementName = "id")]
+        public int Id { get; set; }
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "hiredate")]
+        public DateTime HireDate { get; set; }
+
+        public override string ToString() {
+            return string.Format("[Id={0},Name={1},HireDate={2}]", Id, Name, HireDate);
+        }
+    }
+}
---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var employee = new Employee {
+                Id = 34354,
+                Name = "松倉一輝",
+                HireDate = DateTime.Today,
+            };
+
+            using (var writer = XmlWriter.Create("Employee.xml")) {
+                var serializer = new XmlSerializer(employee.GetType());
+                serializer.Serialize(writer, employee);
+            }
+
+            using (var reader = XmlReader.Create("Employee.xml")) {
+                var serializer = new XmlSerializer(typeof(Employee));
+                employee = serializer.Deserialize(reader) as Employee;
+                Console.WriteLine(employee);
+
+            }
+        }
+    }
+}
