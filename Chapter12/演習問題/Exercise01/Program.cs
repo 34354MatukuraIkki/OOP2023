06ab892fcdd73e041ce738be110367ae7834fe6f
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-04 14:52:26 +0900

    問題12.1.2

--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -9,23 +10,23 @@ using System.Xml.Serialization;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
-
-            var employee = new Employee {
+            var employees = new Employee[] {
+            new Employee {
                 Id = 34354,
                 Name = "松倉一輝",
                 HireDate = DateTime.Today,
-            };
+            },
 
-            using (var writer = XmlWriter.Create("Employee.xml")) {
-                var serializer = new XmlSerializer(employee.GetType());
-                serializer.Serialize(writer, employee);
+            new Employee {
+                Id = 34321,
+                Name = "吉岡亮",
+                HireDate = DateTime.Now,
             }
-
-            using (var reader = XmlReader.Create("Employee.xml")) {
-                var serializer = new XmlSerializer(typeof(Employee));
-                employee = serializer.Deserialize(reader) as Employee;
-                Console.WriteLine(employee);
-
+            };
+        
+            using (var writer = XmlWriter.Create("Employees.xml")) {
+                var serializer = new DataContractSerializer(employees.GetType());
+                serializer.WriteObject(writer, employees);
             }
         }
     }
