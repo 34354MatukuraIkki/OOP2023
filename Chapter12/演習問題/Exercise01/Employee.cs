0f915b1157abef5b1595371d3b3e4277e4d16b11
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-04 15:09:10 +0900

    問題12.1.4

--- Chapter12/演習問題/Exercise01/Employee.cs
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace Exercise01 {
-    [XmlRoot("employee")]
+    [DataContract(Name = "employee")]
     public class Employee {
-        [XmlElement(ElementName = "id")]
+        [XmlIgnore]
         public int Id { get; set; }
-        [XmlElement(ElementName = "name")]
+        [DataMember(Name = "name")]
         public string Name { get; set; }
-        [XmlElement(ElementName = "hiredate")]
+        [DataMember(Name = "hiredate")]
         public DateTime HireDate { get; set; }
 
         public override string ToString() {
--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -10,13 +12,23 @@ using System.Xml.Serialization;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
-            
-            using (var reader = XmlReader.Create("Employees.xml")) {
-                var serializer = new DataContractSerializer(typeof(Employee[]));
-                var employees = serializer.ReadObject(reader) as Employee[];
-                foreach (var employee in employees) {
-                    Console.WriteLine(employee);
-                }
+            var employees = new Employee[] {
+            new Employee {
+                Id = 34354,
+                Name = "松倉一輝",
+                HireDate = DateTime.Today,
+            },
+
+            new Employee {
+                Id = 34321,
+                Name = "吉岡亮",
+                HireDate = DateTime.Now,
+            }
+            };
+
+            using (var stream = new FileStream("employees.json", FileMode.Create, FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer(employees.GetType());
+                serializer.WriteObject(stream, employees);
             }
         }
     }
