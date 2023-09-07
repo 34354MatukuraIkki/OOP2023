89bd50f3a946386d9a92a57231052169b64b2ef4
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-07 10:13:59 +0900

    問題12.2

---
+++ Chapter12/演習問題/Exercise02/Novelist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise02 {
+    [XmlRoot("novelist")]
+    [DataContract]
+    public class Novelist {
+        [XmlElement(ElementName = "name")]
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "birth")]
+        [DataMember(Name = "birth")]
+        public DateTime Birth { get; set; }
+
+        [XmlArray("masterpieces")]
+        [XmlArrayItem("title", typeof(string))]
+        [DataMember(Name = "masterpieces")]
+        public string[] Masterpieces { get; set; }
+    }
+}
---
+++ Chapter12/演習問題/Exercise02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var novelist = Exercise2_1("Sample.xml");
+            Exercise2_2(novelist, "novelist.json");
+
+            //// これは確認のためのコード 12.2.1
+            Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
+            foreach (var title in novelist.Masterpieces) {
+                Console.WriteLine(title);
+            }
+            Console.WriteLine();
+
+            // これは確認のためのコード 12.2.2
+            Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine();
+        }
+
+        private static Novelist Exercise2_1(string v) {
+            using (var reader = XmlReader.Create(v)) {
+                var serializer = new XmlSerializer(typeof(Novelist));
+                var novelist = (Novelist)serializer.Deserialize(reader);
+
+                return novelist;
+            }
+
+        }
+
+        private static void Exercise2_2(Novelist novelist, string v) {
+            using (var stream = new FileStream(v, FileMode.Create, FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer(novelist.GetType(),
+                                    new DataContractJsonSerializerSettings {
+                                        DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                                    });
+                serializer.WriteObject(stream, novelist);
+            }
+        }
+    }
+}
