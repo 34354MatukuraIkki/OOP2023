f038ef6002e0161137b9ecf0003f4090c22a048e
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-26 11:35:33 +0900

    アプリケーションの起動

---
+++ Chapter14/Section01/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Section01 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btRunNotepad = new System.Windows.Forms.Button();
+            this.btRunAndWaitNotepad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btRunNotepad
+            // 
+            this.btRunNotepad.Location = new System.Drawing.Point(144, 141);
+            this.btRunNotepad.Name = "btRunNotepad";
+            this.btRunNotepad.Size = new System.Drawing.Size(330, 116);
+            this.btRunNotepad.TabIndex = 0;
+            this.btRunNotepad.Text = "RunNotepad";
+            this.btRunNotepad.UseVisualStyleBackColor = true;
+            this.btRunNotepad.Click += new System.EventHandler(this.btRunNotepad_Click);
+            // 
+            // btRunAndWaitNotepad
+            // 
+            this.btRunAndWaitNotepad.Location = new System.Drawing.Point(144, 301);
+            this.btRunAndWaitNotepad.Name = "btRunAndWaitNotepad";
+            this.btRunAndWaitNotepad.Size = new System.Drawing.Size(330, 115);
+            this.btRunAndWaitNotepad.TabIndex = 1;
+            this.btRunAndWaitNotepad.Text = "RunAndWaitNotepad";
+            this.btRunAndWaitNotepad.UseVisualStyleBackColor = true;
+            this.btRunAndWaitNotepad.Click += new System.EventHandler(this.btRunAndWaitNotepad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btRunAndWaitNotepad);
+            this.Controls.Add(this.btRunNotepad);
+            this.Name = "Form1";
+            this.Text = "14章１節";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRunNotepad;
+        private System.Windows.Forms.Button btRunAndWaitNotepad;
+    }
+}
+
---
+++ Chapter14/Section01/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Section01 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btRunNotepad_Click(object sender, EventArgs e) {
+            var path = @"%SystemRoot%\system32\notepad.exe";
+            var fullpath = Environment.ExpandEnvironmentVariables(path);
+
+            var startInfo = new ProcessStartInfo {
+                FileName = fullpath,
+                Arguments = @"C:\temp\Sample.txt",
+                WindowStyle = ProcessWindowStyle.Maximized
+            };
+
+            Process.Start(startInfo);
+        }
+
+        private void btRunAndWaitNotepad_Click(object sender, EventArgs e) {
+            RunAndWaitNotepad();
+            MessageBox.Show("終了");
+        }
+
+        private static int RunAndWaitNotepad() {
+            var path = @"%SystemRoot%\system32\notepad.exe";
+            var fullpath = Environment.ExpandEnvironmentVariables(path);
+            using (var process = Process.Start(fullpath)) {
+                if (process.WaitForExit(10000))
+                    return process.ExitCode;
+                throw new TimeoutException();
+            }
+        }
+    }
+}
