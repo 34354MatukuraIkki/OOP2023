c3233df3dc6e127276c34b8fc5babbb0109961c1
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-27 09:23:35 +0900

    DownloadFileAsyncメソッドによる非同期処理

---
+++ Chapter14/Section02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            var wc = new WebClient();
+            var url = new Uri(@"https://www.rimarts.jp/downloads/B2/bk28104j.zip");
+            var filename = @"C:\temp\example.zip";
+            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(url, filename);
+            Console.ReadLine();
+        }
+
+        static void wc_DownloadProgressChanged(object sender,
+                    DownloadProgressChangedEventArgs e) {
+            Console.WriteLine("{0}% {0}/{1}", e.ProgressPercentage,
+                              e.BytesReceived, e.TotalBytesToReceive);
+        }
+
+        static void wc_DownloadFileCompleted(object sender,
+                            System.ComponentModel.AsyncCompletedEventArgs e) {
+            Console.WriteLine("ダウンロード完了");
+        }
+    }
+}
