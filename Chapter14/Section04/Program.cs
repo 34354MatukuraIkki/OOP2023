06d305402f091474252be5f9a26e255b70be4c82
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-27 10:34:51 +0900

    RSSファイルの取得

--- Chapter14/Section04/Program.cs
+++ Chapter14/Section04/Program.cs
@@ -4,20 +4,35 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Section04 {
     class Program {
         static void Main(string[] args) {
-            OpenReadSample();
+            var results = GetWeatherReportFromYahoo(4610);
+            foreach (var s in results) {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
         }
-        //List 14.18
-        private static void OpenReadSample() {
-            var wc = new WebClient();
-            using (var stream = wc.OpenRead(@"http://gihyo.jp/book/list"))
-            using (var sr = new StreamReader(stream, Encoding.UTF8)) {
-                string html = sr.ReadToEnd();
-                Console.WriteLine(html);
+
+        //List 14.19
+        private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"https://news.yahoo.co.jp/rss/topics/it.xml", cityCode);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    string s = Regex.Replace(node.Value, "【|】", "");
+                    yield return node.Value;
+                }
             }
         }
     }
