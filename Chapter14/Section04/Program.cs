9b0770223b37cf8b0a971984dfdf952bdf517a12
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-09-27 10:01:21 +0900

    OpenReadメソッドでWebページを取得する

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            OpenReadSample();
+        }
+        //List 14.18
+        private static void OpenReadSample() {
+            var wc = new WebClient();
+            using (var stream = wc.OpenRead(@"http://gihyo.jp/book/list"))
+            using (var sr = new StreamReader(stream, Encoding.UTF8)) {
+                string html = sr.ReadToEnd();
+                Console.WriteLine(html);
+            }
+        }
+    }
+}
