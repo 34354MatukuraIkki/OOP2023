25aa77dbed5f6d39a35d4c3c562464b6238c09e5
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-04 12:05:24 +0900

    問題10.1

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("080-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("060-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("190-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("091-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-12341"));
+            Console.WriteLine(IsPhoneNumber("A090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-911-1234"));
+            Console.WriteLine(IsPhoneNumber("090-1911-234"));
+        }
+
+        private static bool IsPhoneNumber(string str) {
+            var text = str;
+            bool isMatch = Regex.IsMatch(text, @"^(090)|(080)|(070)-\d{4}-\d{4}$");
+            return isMatch;
+        }
+    }
+}
