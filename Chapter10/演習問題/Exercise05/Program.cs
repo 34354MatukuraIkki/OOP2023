5cab065b90b93ddeef0b37f377ef4ebfd7cc2444
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-10 11:18:29 +0900

    問題10.5

--- Chapter10/演習問題/Exercise05/Program.cs
+++ Chapter10/演習問題/Exercise05/Program.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise05 {
     class Program {
         static void Main(string[] args) {
             TagLower("sample.html");
+
+            var text = File.ReadAllText("sample.html");
+            Console.WriteLine(text);
         }
 
         private static void TagLower(string file) {
             var lines = File.ReadLines(file);
             var sb = new StringBuilder();
-
-
-
-
-
-
+            
+            foreach (var line in lines) {
+                var s = Regex.Replace(line, @"<(/?)([A-Z][A-Z0-9]*)(.*?)>",
+                    m => {
+                    return string.Format("<{0}{1}{2}>", m.Groups[1].Value, m.Groups[2].Value.ToLower(), m.Groups[3].Value);
+                    });
+                sb.AppendLine(s);
+            }
 
             //ファイル出力
             File.WriteAllText(file, sb.ToString());
