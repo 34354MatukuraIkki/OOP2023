49ee03df5e2969f58c26e73b0aad0dcc7eb04404
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-11 11:49:35 +0900

    問題15.2

---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var maxPrice = Library.Books.First(b=>b.Price==Library.Books.Max(m => m.Price));
+            Console.WriteLine(maxPrice);
+        }
+
+        private static void Exercise1_3() {
+           
+        }
+
+        private static void Exercise1_4() {
+        
+        }
+
+        private static void Exercise1_5() {
+        
+        }
+
+        private static void Exercise1_6() {
+        
+        }
+
+        private static void Exercise1_7() {
+        
+        }
+
+        private static void Exercise1_8() {
+        
+        }
+    }
+}
