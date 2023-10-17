3112407d3a4a85aafd4748621f9c1531b0e4b158
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-17 09:38:09 +0900

    問題15.7発行年ごとに分類まで（Developmentのみの出力ができていない）

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -83,10 +83,20 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_7() {
-        
+            var categoryId = Library.Categories.Single(c => c.Name == "Development").Id;
+            var groups = Library.Books
+                                .Where(b => b.CategoryId == categoryId)
+                                .GroupBy(b => b.PublishedYear)
+                                .OrderBy(b => b.Key);
+            foreach (var g in groups) {
+                Console.WriteLine($"#{g.Key}年");
+                foreach (var book in g) {
+                    Console.WriteLine($" {book.Title}");
+                }
+            }
         }
 
-        private static void Exercise1_8() {
+            private static void Exercise1_8() {
         
         }
     }
