c6b118ff10bca31d5516ad6d537da806889631fa
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-17 10:20:48 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter17/Section01/Greeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class GreetingMorning : GreetingBase{
+        public override string GetMessage() {
+            return "おはよう";
+        }
+    }
+
+    class GreetingAfternoon : GreetingBase {
+        public override string GetMessage() {
+            return "こんにちは";
+        }
+    }
+
+    class GreetingEvening : GreetingBase {
+        public override string GetMessage() {
+            return "こんばんは";
+        }
+    }
+}
