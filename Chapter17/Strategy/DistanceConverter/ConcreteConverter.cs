dc42b36a0c03fbdf6fd726283b427388b6cf9a39
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-18 11:58:55 +0900

    TemplateMethodパターン、Strategyパターン

---
+++ Chapter17/Strategy/DistanceConverter/ConcreteConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    // List 17-13 (ConverterBaseの具象クラス)
+
+    public class MeterConverter : ConverterBase {
+        protected override double Ratio { get { return 1; } }
+        public override string UnitName { get { return "メートル"; } }
+
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "meter" || name == UnitName;
+        }
+    }
+
+    public class FeetConverter : ConverterBase {
+        protected override double Ratio { get { return 0.3048; } }
+        public override string UnitName { get { return "フィート"; } }
+
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "feet" || name == UnitName;
+        }
+    }
+
+    public class InchConverter : ConverterBase {
+        protected override double Ratio { get { return 0.0254; } }
+        public override string UnitName { get { return "インチ"; } }
+
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "inch" || name == UnitName;
+        }
+    }
+
+    public class YardConverter : ConverterBase {
+        protected override double Ratio { get { return 0.9144; } }
+        public override string UnitName { get { return "ヤード"; } }
+
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "yard" || name == UnitName;
+        }
+    }
+}
