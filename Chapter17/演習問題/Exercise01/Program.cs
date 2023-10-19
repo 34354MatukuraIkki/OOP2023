35d52f5ceffa1cad107345494d4b83adf129828f
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-19 11:11:35 +0900

    問題17.1

---
+++ Chapter17/演習問題/Exercise01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<ToHankakuProcessor>(@"C:\Users\infosys\source\repos\OOP2023_34354\Chapter17\演習問題\Exercise01\Sample.txt");
+        }
+    }
+}
