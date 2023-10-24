e0c3a35e5ed6310013e6dd05be235261a7e6a3fb
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-10-24 11:55:16 +0900

    WPFのサンプルプログラム01

---
+++ Chapter17/演習問題/Exercise03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var services = new List<ITextFileService>() {
+                new (),
+                new (),
+                new (),
+            };
+
+            //foreach (var obj in greetings) {
+            //    string msg = obj.GetMessage();
+            //    Console.WriteLine(msg);
+            //}
+        }
+    }
+    }
+}
