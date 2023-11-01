a7448bc27b2bbf64cbb6fa99628f453b799eaa7e
Ikki Matukura <OIBC34354@ojs.ac.jp>
2023-11-01 12:08:51 +0900

    colorChecker途中段階

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,14 +19,59 @@ namespace CollarChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        
+        List<MyColor> stockColors = new List<MyColor>();
+
         public MainWindow() {
             InitializeComponent();
+            SetColor();
+
+            DataContext = GetColorList();
+        }
+
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                            .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
 
         private void changeColor(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            Color color = Color.FromArgb(255, (byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value);
+            SetColor();
+        }
+
+        public void SetColor() {
+            Color color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value);
             SolidColorBrush solidColor = new SolidColorBrush(color);
             colorArea.Background = solidColor;
         }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            stockColors.Add(new MyColor() { Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value), Name = $"R = {rValue.Text},G = {gValue.Text},B = {bValue.Text}" });
+
+            foreach (var color in stockColors) {
+                if (!stockList.Items.Contains(color.Name))
+                    stockList.Items.Add(color.Name);
+            }
+        }
+
+        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectColor = (MyColor)sender;
+            var color = selectColor.Color;
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
+        }
+    }
+
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
